84fa405f89f066d33b9fb11ad39f8b74d04089e9
victor <victor.mbuthia@student.moringaschool.com>
2022-08-04 17:11:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string)
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string)
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    console.log(string.toLowerCase());
+    if (string.toLowerCase() === 'hello') 
+        return "I can't hear you!"
+      
+      else if (string ===   string.toUpperCase())
+          return "YES INDEED!";
+
+       else if ('Lets have dinner together!') 
+        return 'I would love to!'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
